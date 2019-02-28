61a2818fcaf3c60bf7a54774615eb4a3f17c49db
Audra Tallman <audra.tallman@hotmail.com>
2019-02-28 19:56:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
-# Add  code here!
+def prime? (number)
+ if (!number.integer?) or number <2 or (2..number-1).each {|factor| number % factor == 0}
+  return false
+  else 
+   return true
+  end
+end
