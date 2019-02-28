6e11bc8992e99151573729c93f5c2a106f2cfb7b
Audra Tallman <audra.tallman@hotmail.com>
2019-02-28 19:57:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
-def prime? (number)
- if (!number.integer?) or number <2 or (2..number-1).each {|factor| number % factor == 0}
-  return false
-  else 
-   return true
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
   end
 end
